3eadc766f6890401455cdc7d832c68111aa99ed7
Ryan Keller <rjkeller_01@hotmail.com>
2021-02-18 18:42:30 -0700

    3 buttons added, 1 for each choice

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
-const btn = document.querySelector('#btn');
+/*const btn = document.querySelector('#btn');
 btn.onclick = () => alert("You picked rock!");
+*/
+function alertFunctionRock() {
+    alert("YAY! YOU picked Rock buddy!");
+  }
+
+  function alertFunctionPaper() {
+    alert("YAY! YOU picked Paper buddy!");
+  }
+  
+  function alertFunctionScissors() {
+    alert("YAY! YOU picked Scissors buddy!");
+  }
+  // METHOD 2
+ // btn.onclick = alertFunction;
+  
+  // METHOD 3
+  //btn.addEventListener('click', alertFunction);
